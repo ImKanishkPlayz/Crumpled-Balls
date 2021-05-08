35e76c9feee0490e42b255a07df0365366f1af9f
ImKanishkPlayz <74292805+ImKanishkPlayz@users.noreply.github.com>
2021-05-08 15:52:57 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,12 +1,20 @@
-class Paper extends BaseClass{
-    constructor(x,y){
-     var options {
-          isStatic : false,
-          restitution:  0.3,
-          friction : 0.5,
-          density : 1.2;
-      }
-      this.image = loadImage("sprites/paper.png");
+class Paper {
+  constructor(x,y) {
+    var options = {
+      isStatic = false,
+      restitution = 0.3,
+      friction = 0,
+      density = 1.2
     }
-    
-  };
+    this.image = loadImage("sprites/paper.png");
+  }
+  display(){
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
--- sketch.js
+++ sketch.js
@@ -37,3 +37,9 @@ function draw() {
 
 }
 
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paperObject.body, paperObject.body.position, {x = 130, y = -145});
+	}
+}
+
